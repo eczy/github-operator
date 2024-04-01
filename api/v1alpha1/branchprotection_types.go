c9325e5155092c674837de15c36b5dbb1fe5476e
Evan Czyzycki <eczy3826@gmail.com>
2024-04-01 12:18:29 -0600

    camelcase for branch protection

--- api/v1alpha1/branchprotection_types.go
+++ api/v1alpha1/branchprotection_types.go
@@ -31,200 +31,200 @@ type BranchProtectionSpec struct {
 	//+kubebuilder:validation:MinLength=1
 
 	// The owner of the repository associated with this branch protection rule.
-	RepositoryOwner string `json:"repository_owner"`
+	RepositoryOwner string `json:"repositoryOwner"`
 
 	//+kubebuilder:validation:MinLength=1
 
 	// The repository associated with this branch protection rule.
-	RepositoryName string `json:"repository_name"`
+	RepositoryName string `json:"repositoryName"`
 
 	//+kubebuilder:validation:MinLength=1
 
 	// Identifies the protection rule pattern.
-	Pattern string `json:"branch_pattern"`
+	Pattern string `json:"branchPattern"`
 
 	// Can this branch be deleted.
 	// +optional
-	AllowsDeletions *bool `json:"allows_deletions,omitempty"`
+	AllowsDeletions *bool `json:"allowsDeletions,omitempty"`
 
 	// Are force pushes allowed on this branch.
 	// +optional
-	AllowsForcePushes *bool `json:"allows_force_pushes,omitempty"`
+	AllowsForcePushes *bool `json:"allowsForcePushes,omitempty"`
 
 	// Is branch creation a protected operation.
 	// +optional
-	BlocksCreations *bool `json:"blocks_creations,omitempty"`
+	BlocksCreations *bool `json:"blocksCreations,omitempty"`
 
 	// A list of users able to force push for this branch protection rule.
 	// +optional
-	BypassForcePushUsers []string `json:"bypass_force_push_users,omitempty"`
+	BypassForcePushUsers []string `json:"bypassForcePushUsers,omitempty"`
 
 	// A list of apps able to force push for this branch protection rule.
 	// +optional
-	BypassForcePushApps []string `json:"bypass_force_push_apps,omitempty"`
+	BypassForcePushApps []string `json:"bypassForcePushApps,omitempty"`
 
 	// A list of teams able to force push for this branch protection rule.
 	// +optional
-	BypassForcePushTeams []string `json:"bypass_force_push_teams,omitempty"`
+	BypassForcePushTeams []string `json:"bypassForcePushTeams,omitempty"`
 
 	// A list of users able to bypass PRs for this branch protection rule.
 	// +optional
-	BypassPullRequestUsers []string `json:"bypass_pull_request_users,omitempty"`
+	BypassPullRequestUsers []string `json:"bypassPullRequestUsers,omitempty"`
 
 	// A list of apps able to bypass PRs for this branch protection rule.
 	// +optional
-	BypassPullRequestApps []string `json:"bypass_pull_request_apps,omitempty"`
+	BypassPullRequestApps []string `json:"bypassPullRequestApps,omitempty"`
 
 	// A list of teams able to bypass PRs for this branch protection rule.
 	// +optional
-	BypassPullRequestTeams []string `json:"bypass_pull_request_teams,omitempty"`
+	BypassPullRequestTeams []string `json:"bypassPullRequestTeams,omitempty"`
 
 	// Will new commits pushed to matching branches dismiss pull request review approvals.
 	// +optional
-	DismissesStaleReviews *bool `json:"dismisses_stale_reviews,omitempty"`
+	DismissesStaleReviews *bool `json:"dismissesStaleReviews,omitempty"`
 
 	// Can admins override branch protection.
 	// +optional
-	IsAdminEnforced *bool `json:"is_admin_enforced,omitempty"`
+	IsAdminEnforced *bool `json:"isAdminEnforced,omitempty"`
 
 	// Whether users can pull changes from upstream when the branch is locked. Set to true to allow fork syncing. Set to false to prevent fork syncing.
 	// +optional
-	LockAllowsFetchAndMerge *bool `json:"lock_allows_fetch_and_merge,omitempty"`
+	LockAllowsFetchAndMerge *bool `json:"lockAllowsFetchAndMerge,omitempty"`
 
 	// Whether to set the branch as read-only. If this is true, users will not be able to push to the branch.
 	// +optional
-	LockBranch *bool `json:"lock_branch,omitempty"`
+	LockBranch *bool `json:"lockBranch,omitempty"`
 
 	// A list of user push allowances for this branch protection rule.
 	// +optional
-	PushAllowanceUsers []string `json:"push_allowance_users,omitempty"`
+	PushAllowanceUsers []string `json:"pushAllowanceUsers,omitempty"`
 
 	// A list of app push allowances for this branch protection rule.
 	// +optional
-	PushAllowanceApps []string `json:"push_allowance_apps,omitempty"`
+	PushAllowanceApps []string `json:"pushAllowanceApps,omitempty"`
 
 	// A list of team push allowances for this branch protection rule.
 	// +optional
-	PushAllowanceTeams []string `json:"push_allowance_teams,omitempty"`
+	PushAllowanceTeams []string `json:"pushAllowanceTeams,omitempty"`
 
 	// Whether the most recent push must be approved by someone other than the person who pushed it.
 	// +optional
-	RequireLastPushApproval *bool `json:"require_last_push_approval,omitempty"`
+	RequireLastPushApproval *bool `json:"requireLastPushApproval,omitempty"`
 
 	// Number of approving reviews required to update matching branches.
 	// +optional
-	RequiredApprovingReviewCount *int `json:"required_approving_review_count,omitempty"`
+	RequiredApprovingReviewCount *int `json:"requiredApprovingReviewCount,omitempty"`
 
 	// List of required deployment environments that must be deployed successfully to update matching branches.
 	// +optional
-	RequiredDeploymentEnvironments []string `json:"required_deployment_environments,omitempty"`
+	RequiredDeploymentEnvironments []string `json:"requiredDeploymentEnvironments,omitempty"`
 
 	// List of required status check contexts that must pass for commits to be accepted to matching branches.
 	// +optional
-	RequiredStatusCheckContexts []string `json:"required_status_check_contexts,omitempty"`
+	RequiredStatusCheckContexts []string `json:"requiredStatusCheckContexts,omitempty"`
 
 	// List of required status checks that must pass for commits to be accepted to matching branches.
 	// +optional
-	RequiredStatusChecks []RequiredStatusCheck `json:"required_status_checks,omitempty"`
+	RequiredStatusChecks []RequiredStatusCheck `json:"requiredStatusChecks,omitempty"`
 
 	// Are approving reviews required to update matching branches.
 	// +optional
-	RequiresApprovingReviews *bool `json:"requires_approving_reviews,omitempty"`
+	RequiresApprovingReviews *bool `json:"requiresApprovingReviews,omitempty"`
 
 	// Are reviews from code owners required to update matching branches.
 	// +optional
-	RequiresCodeOwnerReviews *bool `json:"requires_code_owner_reviews,omitempty"`
+	RequiresCodeOwnerReviews *bool `json:"requiresCodeOwnerReviews,omitempty"`
 
 	// Are commits required to be signed.
 	// +optional
-	RequiresCommitSignatures *bool `json:"requires_commit_signatures,omitempty"`
+	RequiresCommitSignatures *bool `json:"requiresCommitSignatures,omitempty"`
 
 	// Are conversations required to be resolved before merging.
 	// +optional
-	RequiresConversationResolution *bool `json:"requires_conversation_resolution,omitempty"`
+	RequiresConversationResolution *bool `json:"requiresConversationResolution,omitempty"`
 
 	// Does this branch require deployment to specific environments before merging.
 	// +optional
-	RequiresDeployments *bool `json:"requires_deployments,omitempty"`
+	RequiresDeployments *bool `json:"requiresDeployments,omitempty"`
 
 	// Are merge commits prohibited from being pushed to this branch.
 	// +optional
-	RequiresLinearHistory *bool `json:"requires_linear_history,omitempty"`
+	RequiresLinearHistory *bool `json:"requiresLinearHistory,omitempty"`
 
 	// Are status checks required to update matching branches.
 	// +optional
-	RequiresStatusChecks *bool `json:"requires_status_checks,omitempty"`
+	RequiresStatusChecks *bool `json:"requiresStatusChecks,omitempty"`
 
 	// Are branches required to be up to date before merging.
 	// +optional
-	RequiresStrictStatusChecks *bool `json:"requires_strict_status_checks,omitempty"`
+	RequiresStrictStatusChecks *bool `json:"requiresStrictStatusChecks,omitempty"`
 
 	// Is pushing to matching branches restricted.
 	// +optional
-	RestrictsPushes *bool `json:"restricts_pushes,omitempty"`
+	RestrictsPushes *bool `json:"restrictsPushes,omitempty"`
 
 	// Is dismissal of pull request reviews restricted.
 	// +optional
-	RestrictsReviewDismissals *bool `json:"restricts_review_dismissals,omitempty"`
+	RestrictsReviewDismissals *bool `json:"restrictsReviewDismissals,omitempty"`
 
 	// A list of user review dismissal allowances for this branch protection rule.
 	// +optional
-	ReviewDismissalUsers []string `json:"review_dismissal_users,omitempty"`
+	ReviewDismissalUsers []string `json:"reviewDismissalUsers,omitempty"`
 
 	// A list of app review dismissal allowances for this branch protection rule.
 	// +optional
-	ReviewDismissalApps []string `json:"review_dismissal_apps,omitempty"`
+	ReviewDismissalApps []string `json:"reviewDismissalApps,omitempty"`
 
 	// A list of team review dismissal allowances for this branch protection rule.
 	// +optional
-	ReviewDismissalTeams []string `json:"review_dismissal_teams,omitempty"`
+	ReviewDismissalTeams []string `json:"reviewDismissalTeams,omitempty"`
 }
 
 // BranchProtectionStatus defines the observed state of BranchProtection
 type BranchProtectionStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
-	LastUpdateTimestamp *metav1.Time `json:"last_update_timestamp,omitempty"`
+	LastUpdateTimestamp *metav1.Time `json:"lastUpdateTimestamp,omitempty"`
 
-	NodeId                         *string               `json:"node_id,omitempty"`
-	RepositoryNodeId               *string               `json:"repository_node_id,omitempty"` // node id not database id
-	RepositoryOwner                *string               `json:"repository_owner,omitempty"`
-	RepositoryName                 *string               `json:"repository_name,omitempty"`
-	Pattern                        *string               `json:"branch_pattern,omitempty"`
-	AllowsDeletions                *bool                 `json:"allows_deletions,omitempty"`
-	AllowsForcePushes              *bool                 `json:"allows_force_pushes,omitempty"`
-	BlocksCreations                *bool                 `json:"blocks_creations,omitempty"`
-	BypassForcePushUsers           []string              `json:"bypass_force_push_users,omitempty"`
-	BypassForcePushApps            []string              `json:"bypass_force_push_apps,omitempty"`
-	BypassForcePushTeams           []string              `json:"bypass_force_push_teams,omitempty"`
-	BypassPullRequestUsers         []string              `json:"bypass_pull_request_users,omitempty"`
-	BypassPullRequestApps          []string              `json:"bypass_pull_request_apps,omitempty"`
-	BypassPullRequestTeams         []string              `json:"bypass_pull_request_teams,omitempty"`
-	DismissesStaleReviews          *bool                 `json:"dismisses_stale_reviews,omitempty"`
-	IsAdminEnforced                *bool                 `json:"is_admin_enforced,omitempty"`
-	LockAllowsFetchAndMerge        *bool                 `json:"lock_allows_fetch_and_merge,omitempty"`
-	LockBranch                     *bool                 `json:"lock_branch,omitempty"`
-	PushAllowanceUsers             []string              `json:"push_allowance_users,omitempty"`
-	PushAllowanceApps              []string              `json:"push_allowance_apps,omitempty"`
-	PushAllowanceTeams             []string              `json:"push_allowance_teams,omitempty"`
-	RequireLastPushApproval        *bool                 `json:"require_last_push_approval,omitempty"`
-	RequiredApprovingReviewCount   *int                  `json:"required_approving_review_count,omitempty"`
-	RequiredDeploymentEnvironments []string              `json:"required_deployment_environments,omitempty"`
-	RequiredStatusCheckContexts    []string              `json:"required_status_check_contexts,omitempty"`
-	RequiredStatusChecks           []RequiredStatusCheck `json:"required_status_checks,omitempty"`
-	RequiresApprovingReviews       *bool                 `json:"requires_approving_reviews,omitempty"`
-	RequiresCodeOwnerReviews       *bool                 `json:"requires_code_owner_reviews,omitempty"`
-	RequiresCommitSignatures       *bool                 `json:"requires_commit_signatures,omitempty"`
-	RequiresConversationResolution *bool                 `json:"requires_conversation_resolution,omitempty"`
-	RequiresDeployments            *bool                 `json:"requires_deployments,omitempty"`
-	RequiresLinearHistory          *bool                 `json:"requires_linear_history,omitempty"`
-	RequiresStatusChecks           *bool                 `json:"requires_status_checks,omitempty"`
-	RequiresStrictStatusChecks     *bool                 `json:"requires_strict_status_checks,omitempty"`
-	RestrictsPushes                *bool                 `json:"restricts_pushes,omitempty"`
-	RestrictsReviewDismissals      *bool                 `json:"restricts_review_dismissals,omitempty"`
-	ReviewDismissalUsers           []string              `json:"review_dismissal_users,omitempty"`
-	ReviewDismissalApps            []string              `json:"review_dismissal_apps,omitempty"`
-	ReviewDismissalTeams           []string              `json:"review_dismissal_teams,omitempty"`
+	NodeId                         *string               `json:"nodeId,omitempty"`
+	RepositoryNodeId               *string               `json:"repositoryNodeId,omitempty"`
+	RepositoryOwner                *string               `json:"repositoryOwner,omitempty"`
+	RepositoryName                 *string               `json:"repositoryName,omitempty"`
+	Pattern                        *string               `json:"branchPattern,omitempty"`
+	AllowsDeletions                *bool                 `json:"allowsDeletions,omitempty"`
+	AllowsForcePushes              *bool                 `json:"allowsForcePushes,omitempty"`
+	BlocksCreations                *bool                 `json:"blocksCreations,omitempty"`
+	BypassForcePushUsers           []string              `json:"bypassForcePushUsers,omitempty"`
+	BypassForcePushApps            []string              `json:"bypassForcePushApps,omitempty"`
+	BypassForcePushTeams           []string              `json:"bypassForcePushteams,omitempty"`
+	BypassPullRequestUsers         []string              `json:"bypassPullRequestUsers,omitempty"`
+	BypassPullRequestApps          []string              `json:"bypassPullRequestApps,omitempty"`
+	BypassPullRequestTeams         []string              `json:"bypassPullRequestTeams,omitempty"`
+	DismissesStaleReviews          *bool                 `json:"dismissesStaleReviews,omitempty"`
+	IsAdminEnforced                *bool                 `json:"isAdminEnforced,omitempty"`
+	LockAllowsFetchAndMerge        *bool                 `json:"lockAllowsFetchAndMerge,omitempty"`
+	LockBranch                     *bool                 `json:"lockBranch,omitempty"`
+	PushAllowanceUsers             []string              `json:"pushAllowanceUsers,omitempty"`
+	PushAllowanceApps              []string              `json:"pushAllowanceApps,omitempty"`
+	PushAllowanceTeams             []string              `json:"pushAllowanceTeams,omitempty"`
+	RequireLastPushApproval        *bool                 `json:"requireLastPushApproval,omitempty"`
+	RequiredApprovingReviewCount   *int                  `json:"requiredApprovingReviewCount,omitempty"`
+	RequiredDeploymentEnvironments []string              `json:"requiredDeploymentEnvironments,omitempty"`
+	RequiredStatusCheckContexts    []string              `json:"requiredStatusCheckContexts,omitempty"`
+	RequiredStatusChecks           []RequiredStatusCheck `json:"requiredStatusChecks,omitempty"`
+	RequiresApprovingReviews       *bool                 `json:"requiresApprovingReviews,omitempty"`
+	RequiresCodeOwnerReviews       *bool                 `json:"requiresCodeOwnerReviews,omitempty"`
+	RequiresCommitSignatures       *bool                 `json:"requiresCommitSignatures,omitempty"`
+	RequiresConversationResolution *bool                 `json:"requiresConversationResolution,omitempty"`
+	RequiresDeployments            *bool                 `json:"requiresDeployments,omitempty"`
+	RequiresLinearHistory          *bool                 `json:"requiresLinearHistory,omitempty"`
+	RequiresStatusChecks           *bool                 `json:"requiresStatusChecks,omitempty"`
+	RequiresStrictStatusChecks     *bool                 `json:"requiresStrictStatusChecks,omitempty"`
+	RestrictsPushes                *bool                 `json:"restrictsPushes,omitempty"`
+	RestrictsReviewDismissals      *bool                 `json:"restrictsReviewDismissals,omitempty"`
+	ReviewDismissalUsers           []string              `json:"reviewDismissalUsers,omitempty"`
+	ReviewDismissalApps            []string              `json:"reviewDismissalApps,omitempty"`
+	ReviewDismissalTeams           []string              `json:"reviewDismissalTeams,omitempty"`
 }
 
 //+kubebuilder:object:root=true
@@ -253,6 +253,6 @@ func init() {
 }
 
 type RequiredStatusCheck struct {
-	AppId   *string `json:"app_id,omitempty"`
+	AppId   *string `json:"appId,omitempty"`
 	Context string  `json:"context"`
 }
