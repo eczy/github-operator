4cd1b422c3db38e784966c84adfe4dba5d654b92
Evan Czyzycki <eczy3826@gmail.com>
2024-12-23 17:37:13 -0600

    Update via kubebuilder alpha generate

--- api/v1alpha1/organization_types.go
+++ api/v1alpha1/organization_types.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2024 Evan Czyzycki
+Copyright 2024.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -23,167 +23,25 @@ import (
 // EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
-// OrganizationSpec defines the desired state of Organization
+// OrganizationSpec defines the desired state of Organization.
 type OrganizationSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
 
-	//+kubebuilder:validation:MinLength=1
-
-	// The organization name. The name is not case sensitive.
-	Login string `json:"login"`
-
-	//+kubebuilder:validation:MinLength=1
-
-	// The shorthand name of the company.
-	// +optional
-	Name *string `json:"name,omitempty"`
-
-	//+kubebuilder:validation:MinLength=1
-
-	// Billing email address. This address is not publicized.
-	// +optional
-	BillingEmail *string `json:"billingEmail,omitempty"`
-
-	//+kubebuilder:validation:MinLength=1
-
-	// The company name.
-	// +optional
-	Company *string `json:"company,omitempty"`
-
-	// The publicly visible email address.
-	// +optional
-	Email *string `json:"email,omitempty"`
-
-	// The Twitter username of the company.
-	// +optional
-	TwitterUsername *string `json:"twitterUsername,omitempty"`
-
-	// The location.
-	// +optional
-	Location *string `json:"location,omitempty"`
-
-	// The description of the company.
-	// +optional
-	Description *string `json:"description,omitempty"`
-
-	// Whether an organization can use organization projects.
-	// +optional
-	HasOrganizationProjects *bool `json:"hasOrganizationProjects,omitempty"`
-
-	// Whether repositories that belong to the organization can use repository projects.
-	// +optional
-	HasRepositoryProjects *bool `json:"hasRepositoryProjects,omitempty"`
-
-	// Default permission level members have for organization repositories.
-	// Can be one of: read, write, admin, none
-	// +optional
-	DefaultRepositoryPermission *DefaultRepositoryPermission `json:"defaultRepositoryPermission,omitempty"`
-
-	// Whether of non-admin organization members can create repositories.
-	// +optional
-	MembersCanCreateRepositories *bool `json:"membersCanCreateRepositories,omitempty"`
-
-	// Whether organization members can create internal repositories, which are visible to all enterprise members. You can only allow members to create internal repositories if your organization is associated with an enterprise account using GitHub Enterprise Cloud or GitHub Enterprise Server 2.20+.
-	// +optional
-	MembersCanCreateInternalRepositories *bool `json:"membersCanCreateInternalRepositories,omitempty"`
-
-	// Whether organization members can create private repositories, which are visible to organization members with permission.
-	// +optional
-	MembersCanCreatePrivateRepositories *bool `json:"membersCanCreatePrivateRepositories,omitempty"`
-
-	// Whether organization members can create public repositories, which are visible to anyone.
-	// +optional
-	MembersCanCreatePublicRepositories *bool `json:"membersCanCreatePublicRepositories,omitempty"`
-
-	// Whether organization members can create GitHub Pages sites.
-	// +optional
-	MembersCanCreatePages *bool `json:"membersCanCreatePages,omitempty"`
-
-	// Whether organization members can create public GitHub Pages sites.
-	// +optional
-	MembersCanCreatePublicPages *bool `json:"membersCanCreatePublicPages,omitempty"`
-
-	// Whether organization members can create private GitHub Pages sites.
-	// +optional
-	MembersCanCreatePrivatePages *bool `json:"membersCanCreatePrivatePages,omitempty"`
-
-	// Whether organization members can create private GitHub Pages sites.
-	// +optional
-	MembersCanForkPrivateRepositories *bool `json:"membersCanForkPrivateRepositories,omitempty"`
-
-	// Whether contributors to organization repositories are required to sign off on commits they make through GitHub's web interface.
-	// +optional
-	WebCommitSignoffRequired *bool `json:"webCommitSignoffRequired,omitempty"`
-
-	// +optional
-	Blog *string `json:"blog,omitempty"`
-
-	// Whether GitHub Advanced Security is automatically enabled for new repositories.
-	// +optional
-	AdvancedSecurityEnabledForNewRepositories *bool `json:"advancedSecurityEnabledForNewRepositories,omitempty"`
-
-	// Whether Dependabot alerts is automatically enabled for new repositories.
-	// +optional
-	DependabotAlertsEnabledForNewRepositories *bool `json:"dependabotAlertsEnabledForNewRepositories,omitempty"`
-
-	// Whether Dependabot security updates is automatically enabled for new repositories.
-	// +optional
-	DependabotSecurityUpdatesEnabledForNewRepositories *bool `json:"dependabotSecurityUpdatesEnabledForNewRepositories,omitempty"`
-
-	// Whether dependency graph is automatically enabled for new repositories.
-	// +optional
-	DependencyGraphEnabledForNewRepositories *bool `json:"dependencyGraphEnabledForNewRepositories,omitempty"`
-
-	// Whether secret scanning is automatically enabled for new repositories.
-	// +optional
-	SecretScanningEnabledForNewRepositories *bool `json:"secretScanningEnabledForNewRepositories,omitempty"`
-
-	// Whether secret scanning push protection is automatically enabled for new repositories.
-	// +optional
-	SecretScanningPushProtectionEnabledForNewRepositories *bool `json:"secretScanningPushProtectionEnabledForNewRepositories,omitempty"`
+	// Foo is an example field of Organization. Edit organization_types.go to remove/update
+	Foo string `json:"foo,omitempty"`
 }
 
-// OrganizationStatus defines the observed state of Organization
+// OrganizationStatus defines the observed state of Organization.
 type OrganizationStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
-	Login               *string      `json:"login,omitempty"`
-	NodeId              *string      `json:"nodeId,omitempty"`
-	LastUpdateTimestamp *metav1.Time `json:"lastUpdateTimestamp,omitempty"`
-
-	Name                                                  string                       `json:"name"`
-	BillingEmail                                          string                       `json:"billingEmail,omitempty"`
-	Company                                               string                       `json:"company,omitempty"`
-	Email                                                 string                       `json:"email"`
-	TwitterUsername                                       *string                      `json:"twitterUsername,omitempty"`
-	Location                                              *string                      `json:"location,omitempty"`
-	Description                                           *string                      `json:"description,omitempty"`
-	HasOrganizationProjects                               *bool                        `json:"hasOrganizationProjects,omitempty"`
-	HasRepositoryProjects                                 *bool                        `json:"hasRepositoryProjects,omitempty"`
-	DefaultRepositoryPermission                           *DefaultRepositoryPermission `json:"defaultRepositoryPermission,omitempty"`
-	MembersCanCreateRepositories                          *bool                        `json:"membersCanCreateRepositories,omitempty"`
-	MembersCanCreateInternalRepositories                  *bool                        `json:"membersCanCreateInternalRepositories,omitempty"`
-	MembersCanCreatePrivateRepositories                   *bool                        `json:"membersCanCreatePrivateRepositories,omitempty"`
-	MembersCanCreatePublicRepositories                    *bool                        `json:"membersCanCreatePublicRepositories,omitempty"`
-	MembersCanCreatePages                                 *bool                        `json:"membersCanCreatePages,omitempty"`
-	MembersCanCreatePublicPages                           *bool                        `json:"membersCanCreatePublicPages,omitempty"`
-	MembersCanCreatePrivatePages                          *bool                        `json:"membersCanCreatePrivatePages,omitempty"`
-	MembersCanForkPrivateRepositories                     *bool                        `json:"membersCanForkPrivateRepositories,omitempty"`
-	WebCommitSignoffRequired                              *bool                        `json:"webCommitSignoffRequired,omitempty"`
-	Blog                                                  *string                      `json:"blog,omitempty"`
-	AdvancedSecurityEnabledForNewRepositories             *bool                        `json:"advancedSecurityEnabledForNewRepositories,omitempty"`
-	DependabotAlertsEnabledForNewRepositories             *bool                        `json:"dependabotAlertsEnabledForNewRepositories,omitempty"`
-	DependabotSecurityUpdatesEnabledForNewRepositories    *bool                        `json:"dependabotSecurityUpdatesEnabledForNewRepositories,omitempty"`
-	DependencyGraphEnabledForNewRepositories              *bool                        `json:"dependencyFraphEnabledForNewRepositories,omitempty"`
-	SecretScanningEnabledForNewRepositories               *bool                        `json:"secretScanningEnabledForNewRepositories,omitempty"`
-	SecretScanningPushProtectionEnabledForNewRepositories *bool                        `json:"secretScanningPushProtectionEnabledForNewRepositories,omitempty"`
 }
 
-//+kubebuilder:object:root=true
-//+kubebuilder:subresource:status
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
 
-// Organization is the Schema for the organizations API
+// Organization is the Schema for the organizations API.
 type Organization struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
@@ -192,9 +50,9 @@ type Organization struct {
 	Status OrganizationStatus `json:"status,omitempty"`
 }
 
-//+kubebuilder:object:root=true
+// +kubebuilder:object:root=true
 
-// OrganizationList contains a list of Organization
+// OrganizationList contains a list of Organization.
 type OrganizationList struct {
 	metav1.TypeMeta `json:",inline"`
 	metav1.ListMeta `json:"metadata,omitempty"`
@@ -204,13 +62,3 @@ type OrganizationList struct {
 func init() {
 	SchemeBuilder.Register(&Organization{}, &OrganizationList{})
 }
-
-// +kubebuilder:validation:Enum=read;write;none;admin
-type DefaultRepositoryPermission string
-
-const (
-	DefaultRepositoryPermissionRead  DefaultRepositoryPermission = "read"
-	DefaultRepositoryPermissionWrite DefaultRepositoryPermission = "write"
-	DefaultRepositoryPermissionNone  DefaultRepositoryPermission = "none"
-	DefaultRepositoryPermissionAdmin DefaultRepositoryPermission = "admin"
-)
--- internal/controller/team_controller.go
+++ internal/controller/team_controller.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2024 Evan Czyzycki
+Copyright 2024.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -18,54 +18,24 @@ package controller
 
 import (
 	"context"
-	"fmt"
-	"time"
 
-	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
-	githubv1alpha1 "github.com/eczy/github-operator/api/v1alpha1"
-	gh "github.com/eczy/github-operator/internal/github"
-	"github.com/google/go-github/v60/github"
+	githubv1 "github.com/eczy/github-operator/api/v1"
 )
-
-var (
-	teamFinalizerName = "github.github-operator.eczy.io/team-finalizer"
-)
-
-type TeamRequester interface {
-	GetTeamBySlug(ctx context.Context, org, slug string) (*github.Team, error)
-	GetTeamById(ctx context.Context, org, teamId int64) (*github.Team, error)
-	GetTeamByNodeId(ctx context.Context, nodeId string) (*github.Team, error)
-
-	CreateTeam(ctx context.Context, org string, newTeam github.NewTeam) (*github.Team, error)
-	UpdateTeamBySlug(ctx context.Context, org, slug string, newTeam github.NewTeam) (*github.Team, error)
-	UpdateTeamById(ctx context.Context, org, teamId int64, newTeam github.NewTeam) (*github.Team, error)
-	DeleteTeamBySlug(ctx context.Context, org, slug string) error
-	DeleteTeamById(ctx context.Context, org, teamId int64) error
-
-	GetTeamRepositoryPermission(ctx context.Context, org, slug, repoName string) (*gh.TeamRepositoryPermission, error)
-	GetTeamRepositoryPermissions(ctx context.Context, org, slug string) ([]*gh.TeamRepositoryPermission, error)
-	UpdateTeamRepositoryPermissions(ctx context.Context, org, slug string, repoName, permission string) error
-	RemoveTeamRepositoryPermissions(ctx context.Context, org, slug string, repoName string) error
-}
 
 // TeamReconciler reconciles a Team object
 type TeamReconciler struct {
 	client.Client
-	Scheme                   *runtime.Scheme
-	GitHubClient             TeamRequester
-	DeleteOnResourceDeletion bool
-	RequeueInterval          time.Duration
+	Scheme *runtime.Scheme
 }
 
-//+kubebuilder:rbac:groups=github.github-operator.eczy.io,resources=teams,verbs=get;list;watch;create;update;patch;delete
-//+kubebuilder:rbac:groups=github.github-operator.eczy.io,resources=teams/status,verbs=get;update;patch
-//+kubebuilder:rbac:groups=github.github-operator.eczy.io,resources=teams/finalizers,verbs=update
+// +kubebuilder:rbac:groups=github.github-operator.eczy.io,resources=teams,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=github.github-operator.eczy.io,resources=teams/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=github.github-operator.eczy.io,resources=teams/finalizers,verbs=update
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
@@ -75,286 +45,19 @@ type TeamReconciler struct {
 // the user.
 //
 // For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.17.0/pkg/reconcile
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.19.1/pkg/reconcile
 func (r *TeamReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	log := log.FromContext(ctx)
+	_ = log.FromContext(ctx)
 
-	if r.GitHubClient == nil {
-		return ctrl.Result{}, fmt.Errorf("nil GitHub client")
-	}
+	// TODO(user): your logic here
 
-	// fetch resource
-	team := &githubv1alpha1.Team{}
-	if err := r.Get(ctx, req.NamespacedName, team); err != nil {
-		log.Error(err, "error fetching Team resource")
-		return ctrl.Result{}, client.IgnoreNotFound(err)
-	}
-
-	var observed *github.Team
-	// try to fetch external resource
-	if team.Status.NodeId != nil {
-		ghTeam, err := r.GitHubClient.GetTeamByNodeId(ctx, *team.Status.NodeId)
-		if _, ok := err.(*gh.TeamNotFoundError); ok {
-			log.Info(err.Error())
-		} else if err != nil {
-			log.Error(err, "error fetching GitHub team")
-			return ctrl.Result{}, err
-		}
-		observed = ghTeam
-	} else {
-		ghTeam, err := r.GitHubClient.GetTeamBySlug(ctx, team.Spec.Organization, team.Spec.Name)
-		if _, ok := err.(*gh.TeamNotFoundError); ok {
-			log.Info(err.Error())
-		} else if err != nil {
-			log.Error(err, "error fetching GitHub team")
-			return ctrl.Result{}, err
-		}
-		observed = ghTeam
-	}
-
-	// if external resource does't exist and we aren't deleting the resource, create external resource
-	if observed == nil && team.ObjectMeta.DeletionTimestamp.IsZero() {
-		log.Info("creating team", "name", team.Spec.Name)
-		ghTeam, err := r.createTeam(ctx, team)
-		if err != nil {
-			log.Error(err, "error creating GitHub team")
-			return ctrl.Result{}, err
-		}
-		observed = ghTeam
-	}
-
-	// handle finalizer
-	if r.DeleteOnResourceDeletion {
-		if team.ObjectMeta.DeletionTimestamp.IsZero() {
-			// not being deleted
-			if !controllerutil.ContainsFinalizer(team, teamFinalizerName) {
-				controllerutil.AddFinalizer(team, teamFinalizerName)
-				if err := r.Update(ctx, team); err != nil {
-					return ctrl.Result{}, err
-				}
-			}
-		} else {
-			// being deleted
-			if team.Status.LastUpdateTimestamp != nil {
-				// if we have never resolved this resource before, don't
-				// touch external state
-				if err := r.deleteTeam(ctx, team); err != nil {
-					log.Error(err, "unable to delete team")
-					return ctrl.Result{}, err
-				}
-			}
-
-			controllerutil.RemoveFinalizer(team, teamFinalizerName)
-			if err := r.Update(ctx, team); err != nil {
-				return ctrl.Result{}, err
-			}
-
-			return ctrl.Result{}, nil
-		}
-	}
-
-	// update external resource
-	err := r.updateTeam(ctx, team, observed)
-	if err != nil {
-		return ctrl.Result{}, err
-	}
-
-	return ctrl.Result{RequeueAfter: r.RequeueInterval}, nil
+	return ctrl.Result{}, nil
 }
 
 // SetupWithManager sets up the controller with the Manager.
 func (r *TeamReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
-		For(&githubv1alpha1.Team{}).
+		For(&githubv1.Team{}).
+		Named("team").
 		Complete(r)
-}
-
-func (r *TeamReconciler) createTeam(ctx context.Context, team *githubv1alpha1.Team) (*github.Team, error) {
-	newTeam := teamResourceToNewTeam(team)
-	created, err := r.GitHubClient.CreateTeam(ctx, team.Spec.Organization, newTeam)
-	if err != nil {
-		return nil, fmt.Errorf("creating GitHub Team: %w", err)
-	}
-	return created, nil
-}
-
-// modifies team and ghTeam in place
-func (r *TeamReconciler) updateTeam(ctx context.Context, team *githubv1alpha1.Team, ghTeam *github.Team) error {
-	log := log.FromContext(ctx)
-
-	updateTeam := github.NewTeam{}
-	needsUpdate := false
-
-	// resolve name
-	// name can never be blank
-	updateTeam.Name = team.Spec.Name
-	if team.Spec.Name != ghTeam.GetName() {
-		log.Info("team name update", "from", ghTeam.GetName(), "to", team.Spec.Name)
-		needsUpdate = true
-	}
-	if team.Spec.Name != team.GetObjectMeta().GetName() {
-		log.Info("team spec Name does not match metadata Name", "spec", team.Spec.Name, "metadata", team.GetObjectMeta().GetName())
-	}
-
-	// resolve description
-	if ptrNonNilAndNotEqualTo(team.Spec.Description, ghTeam.GetDescription()) {
-		updateTeam.Description = team.Spec.Description
-		needsUpdate = true
-		log.Info("team description update", "from", ghTeam.GetDescription(), "to", *team.Spec.Description, "name", team.Spec.Name)
-
-	}
-
-	// resolve privacy
-	if ptrNonNilAndNotEqualTo(team.Spec.Privacy, githubv1alpha1.Privacy(ghTeam.GetPrivacy())) {
-		updateTeam.Privacy = (*string)(team.Spec.Privacy)
-		needsUpdate = true
-		log.Info("team privacy update", "from", githubv1alpha1.Privacy(ghTeam.GetPrivacy()), "to", *team.Spec.Privacy, "name", team.Spec.Name)
-
-	}
-
-	// resolve parent
-	parent := ghTeam.GetParent()
-	if parent != nil {
-		if ptrNonNilAndNotEqualTo(team.Spec.ParentTeamId, parent.GetID()) {
-			updateTeam.ParentTeamID = team.Spec.ParentTeamId
-			needsUpdate = true
-			log.Info("team parent update", "from", parent.GetID(), "to", team.Spec.ParentTeamId, "name", team.Spec.Name)
-
-		} else if team.Spec.ParentTeamId == nil {
-			updateTeam.ParentTeamID = nil
-			needsUpdate = true
-			log.Info("team parent update", "from", parent.GetID(), "to", team.Spec.ParentTeamId, "name", team.Spec.Name)
-		}
-	} else if team.Spec.ParentTeamId != nil {
-		updateTeam.ParentTeamID = team.Spec.ParentTeamId
-		needsUpdate = true
-		log.Info("team parent update", "from", nil, "to", team.Spec.ParentTeamId, "name", team.Spec.Name)
-	}
-
-	// TODO: team members and maintainers
-
-	// perform update if necessary
-	if needsUpdate || team.Status.LastUpdateTimestamp == nil {
-		log.Info("updating team", "name", team.Spec.Name)
-		updated, err := r.GitHubClient.UpdateTeamById(ctx, *ghTeam.Organization.ID, *ghTeam.ID, updateTeam)
-		if err != nil {
-			log.Error(err, "error updating team", "name", team.Spec.Name)
-			return err
-		}
-		ghTeam = updated
-
-		now := v1.Now()
-		parent := ghTeam.GetParent()
-		var parentId *int64
-		var parentSlug *string
-		if parent != nil {
-			parentId = parent.ID
-			parentSlug = parent.Slug
-		}
-		team.Status = githubv1alpha1.TeamStatus{
-			NodeId:              ghTeam.NodeID,
-			Id:                  ghTeam.ID,
-			Slug:                ghTeam.Slug,
-			LastUpdateTimestamp: &now,
-			OrganizationLogin:   github.String(ghTeam.GetOrganization().GetLogin()),
-			OrganizationId:      github.Int64(ghTeam.GetOrganization().GetID()),
-			Name:                ghTeam.Name,
-			Description:         ghTeam.Description,
-			// TODO
-			// Members:             []string{},
-			// Maintainers:         []string{},
-			Privacy: (*githubv1alpha1.Privacy)(ghTeam.Privacy),
-			// TODO
-			// NotificationSetting: &"",
-			ParentTeamId:   parentId,
-			ParentTeamSlug: parentSlug,
-		}
-
-		// update status
-		if err := r.Status().Update(ctx, team); err != nil {
-			log.Error(err, "error updating Team status", "name", team.Spec.Name)
-		}
-	}
-
-	// Repositories
-	log.Info("updating team repository permissions")
-	trps, err := r.GitHubClient.GetTeamRepositoryPermissions(ctx, ghTeam.GetOrganization().GetLogin(), ghTeam.GetSlug())
-	if err != nil {
-		log.Error(err, "error getting team repository permissions")
-		return err
-	}
-
-	statusRepoPermissions := map[string]githubv1alpha1.RepositoryPermission{}
-	needsUpdate = false
-
-	for _, trp := range trps {
-		if permission, ok := team.Spec.Repositories[trp.RepositoryName]; ok {
-			if permission != githubv1alpha1.RepositoryPermission(trp.Permission) {
-				log.Info("updating team repository permission", "team", team.GetName(), "repository", trp.RepositoryName, "permission", permission)
-				err := r.GitHubClient.UpdateTeamRepositoryPermissions(ctx, ghTeam.GetOrganization().GetLogin(), ghTeam.GetSlug(), trp.RepositoryName, string(permission))
-				if err != nil {
-					log.Error(err, "error updating team repository permissions")
-					return err
-				}
-				statusRepoPermissions[trp.RepositoryName] = permission
-				needsUpdate = true
-			}
-		} else {
-			log.Info("removing team repository permission", "team", team.GetName(), "repository", trp.RepositoryName)
-			err := r.GitHubClient.RemoveTeamRepositoryPermissions(ctx, ghTeam.GetOrganization().GetLogin(), ghTeam.GetSlug(), trp.RepositoryName)
-			if err != nil {
-				log.Error(err, "error removing team repository permissions")
-				return err
-			}
-			needsUpdate = true
-		}
-	}
-
-	for repository, permission := range team.Spec.Repositories {
-		if _, ok := statusRepoPermissions[repository]; !ok {
-			err := r.GitHubClient.UpdateTeamRepositoryPermissions(ctx, ghTeam.GetOrganization().GetLogin(), ghTeam.GetSlug(), repository, string(permission))
-			if err != nil {
-				log.Error(err, "error updating team repository permissions")
-				return err
-			}
-			statusRepoPermissions[repository] = permission
-			needsUpdate = true
-		}
-	}
-
-	if needsUpdate {
-		team.Status.Repositories = statusRepoPermissions
-		// update status
-		if err := r.Status().Update(ctx, team); err != nil {
-			log.Error(err, "error updating Team status", "name", team.Spec.Name)
-		}
-	}
-
-	return nil
-}
-
-func (r *TeamReconciler) deleteTeam(ctx context.Context, team *githubv1alpha1.Team) error {
-	if team.Status.OrganizationLogin == nil {
-		return fmt.Errorf("team OrganizationLogin nil")
-	}
-	if team.Status.Slug == nil {
-		return fmt.Errorf("team Slug is nil")
-	}
-	return r.GitHubClient.DeleteTeamBySlug(ctx, *team.Status.OrganizationLogin, *team.Status.Slug)
-}
-
-// teamResourceToNewTeam creates a github.NewTeam instance from a Team resource
-func teamResourceToNewTeam(team *githubv1alpha1.Team) github.NewTeam {
-	var privacy *string
-	if team.Spec.Privacy != nil {
-		tmp := string(*team.Spec.Privacy)
-		privacy = &tmp
-	}
-	newTeam := github.NewTeam{
-		Name:         team.Spec.Name,
-		Description:  team.Spec.Description,
-		ParentTeamID: team.Spec.ParentTeamId,
-		Privacy:      privacy,
-	}
-	return newTeam
 }
